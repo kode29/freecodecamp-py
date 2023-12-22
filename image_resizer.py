8c4a221df4db71011468a00b857f6764920e0fa9
Kyle Perkins <kyperkins@gmail.com>
2023-12-22 00:39:35 -0500

    add input, ignore aspect

--- image_resizer.py
+++ image_resizer.py
@@ -2,8 +2,17 @@ from PIL import Image
 
 image = Image.open("soccer_practice.jpg")
 
+
+def resize_image(size1, size2):
+    resized_image = image.resize((size1, size2))
+
+    # NOTE: Does NOT incorporate the aspect ratio/proportion
+    resized_image.save(f"soccer-{size1}x{size2}.jpg")
+    print(f"Image resized to {size1}x{size2}")
+
 print(f"Current Size: {image.size}")
 
-resized_image = image.resize((500,500))
+size1 = int(input("Enter Width: "))
+size2 = int(input("Enter Height: "))
 
-resized_image.save('soccer-500.jpg')
+resize_image(size1, size2)
